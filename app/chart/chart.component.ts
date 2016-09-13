02c818bef909cbd8f189abd930d6951267104370
kenkla <ken@kenklatt.com>
2016-09-12 23:03:22 -0400

    add chart module

---
+++ app/chart/chart.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'chart-output',
+  template: './chart.component.html',
+})
+
+export class ChartComponent { }
--- app/chart/chart.module.js
+++ app/chart/chart.module.js
@@ -9,23 +9,20 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
-var platform_browser_1 = require('@angular/platform-browser');
-/* App Root */
-var app_component_1 = require('./app.component');
-/* Feature Modules */
-var chart_module_1 = require('./chart/chart.module');
-var AppModule = (function () {
-    function AppModule() {
+var common_1 = require('@angular/common');
+var chart_component_1 = require('./chart.component');
+var ChartModule = (function () {
+    function ChartModule() {
     }
-    AppModule = __decorate([
+    ChartModule = __decorate([
         core_1.NgModule({
-            imports: [platform_browser_1.BrowserModule, chart_module_1.ChartModule],
-            declarations: [app_component_1.AppComponent],
-            bootstrap: [app_component_1.AppComponent]
+            imports: [common_1.CommonModule],
+            declarations: [chart_component_1.ChartComponent],
+            exports: [chart_component_1.ChartComponent]
         }), 
         __metadata('design:paramtypes', [])
-    ], AppModule);
-    return AppModule;
+    ], ChartModule);
+    return ChartModule;
 }());
-exports.AppModule = AppModule;
-//# sourceMappingURL=app.module.js.map
+exports.ChartModule = ChartModule;
+//# sourceMappingURL=chart.module.js.map
