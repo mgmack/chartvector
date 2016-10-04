4bbeb82095ab230007b49904649482aea07d89a8
kenkla <ken@kenklatt.com>
2016-10-03 22:33:46 -0400

    added getZillowData method

--- app/service/quandl.service.js
+++ app/service/quandl.service.js
@@ -10,12 +10,24 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 var core_1 = require('@angular/core');
 var http_1 = require('@angular/http');
+require('rxjs/add/operator/map');
 require('rxjs/add/operator/toPromise');
 var QuandlService = (function () {
     function QuandlService(http) {
         this.http = http;
         this.endpoint_url = "https://www.quandl.com/api/v3/datasets/";
+        this.url_query_string = ".json?api_key=";
+        this.quandl_api_key = "1oC8WM9amPy7r5xsctjr";
+        this.zillow_path = "ZILL/";
     }
+    QuandlService.prototype.getZillowData = function (area, areacode, indicator) {
+        /* for documentation, see https://www.quandl.com/data/ZILL/documentation/documentation */
+        return this.http.get(this.endpoint_url +
+            this.zillow_path +
+            area + areacode + "_" + indicator +
+            this.url_query_string +
+            this.quandl_api_key).map(function (res) { return res.json(); });
+    };
     QuandlService = __decorate([
         core_1.Injectable(), 
         __metadata('design:paramtypes', [http_1.Http])
