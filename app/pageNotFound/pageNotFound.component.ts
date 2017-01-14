4479fc14195124315cadbf6e0474c597328b74f7
kenkla <ken@kenklatt.com>
2017-01-14 15:05:01 -0500

    change folder structure

--- app/pageNotFound/pageNotFound.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  moduleId: module.id,
-  selector: 'pageNotFound',
-  templateUrl: 'pageNotFound.html',
-})
-
-export class PageNotFoundComponent { }
